5509cd8667c6dc5610d7609298beca1813eb95f7
Valiant Mi-ing <2212579@slu.edu.ph>
2023-12-09 02:39:56 +0800

    requester (Upload and Offices)

--- requester-add.php
+++ requester-add.php
@@ -16,6 +16,7 @@ include('includes/requester.php');
   <link rel="icon" type="image/png" href="assets/slu_logo.png">
   <!-- MAIN CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
   <link href="resources/css/user-home.css" rel="stylesheet">
   <link href="resources/css/requester-add.css" rel="stylesheet">
 </head>
@@ -95,101 +96,81 @@ include('includes/requester.php');
     <div class="home-content">
       <div class="wrapper">
         <form action="includes/upload.php" method="post" enctype="multipart/form-data">
-          <input class="file-input" type="file" name="file" accept=".doc, .docx, .pdf" hidden>
-          <ion-icon name="cloud-upload-outline"></ion-icon>
-          <p>Browse File to Upload</p><br>
+          <div class="file-upload">
+            <input class="file-input" type="file" name="file" accept=".doc, .docx, .pdf" hidden>
+            <ion-icon name="cloud-upload-outline"></ion-icon>
+            <p>Browse File to Upload</p><br>
+          </div>
           <button type="submit" name="upload">Submit</button>
         </form>
+        <div>
+        <div class="circle-container">
+                        <div class="sequenceTitle">
+                            <p>Select the Sequence of Review</p>
+                        </div>
+                        <?php
+                        $numSections = 5;
+                        for ($i = 1; $i <= $numSections; $i++) {
+                            ?>
+                            <div class="seq">
+                                <div class="circle"><?php echo $i; ?></div>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Reviwer
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <?php
+                                        $sql = "SELECT DISTINCT officeName FROM organization";
+                                        $result = $conn->query($sql);
+
+                                        if ($result->num_rows > 0) {
+                                            while ($row = $result->fetch_assoc()) {
+                                                $officeName = $row["officeName"];
+                                                echo "<li><a class='dropdown-item'>$officeName</a></li>";
+                                            }
+                                        }
+                                        ?>
+                                    </ul>
+                                </div>
+                                <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Name of Reviewer
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item">Valiant</a></li>
+                                    <li><a class="dropdown-item">Kenshin</a></li>
+                                    <li><a class="dropdown-item">Faye</a></li>
+                                </ul>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+
         <section class="progress-area"></section>
         <section class="uploaded-area"></section>
-        <div class="circle-container">
-          <div class="sequenceTitle">
-            <p>Select the Sequence of Review</p>
-          </div>
-          <div class="seq">
-            <div class="circle">1</div>
-            <div class="btn-group">
-              <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Reviwer
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Unit</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Academic Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Finance</a></li>
-                <li><a class="dropdown-item" href="#">Office for Legal Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Administration</a></li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="seq">
-            <div class="circle">2</div>
-            <div class="btn-group">
-              <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Reviwer
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Unit</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Academic Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Finance</a></li>
-                <li><a class="dropdown-item" href="#">Office for Legal Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Administration</a></li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="seq">
-            <div class="circle">3</div>
-            <div class="btn-group">
-              <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Reviwer
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Unit</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Academic Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Finance</a></li>
-                <li><a class="dropdown-item" href="#">Office for Legal Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Administration</a></li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="seq">
-            <div class="circle">4</div>
-            <div class="btn-group">
-              <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Reviwer
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Unit</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Academic Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Finance</a></li>
-                <li><a class="dropdown-item" href="#">Office for Legal Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Administration</a></li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="seq">
-            <div class="circle">5</div>
-            <div class="btn-group">
-              <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Reviwer
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Unit</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Academic Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Finance</a></li>
-                <li><a class="dropdown-item" href="#">Office for Legal Affairs</a></li>
-                <li><a class="dropdown-item" href="#">Office of the Vice President for Administration</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
       </div>
   </section>
 
-
+  <div class="modal fade" id="uploadModal" tabindex="-1" role="dialog" aria-labelledby="uploadModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="uploadModalLabel">File Upload Status</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                File Uploaded Successfully
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal" id="closeModalButton">Close</button>
+            </div>
+        </div>
+    </div>
+  </div>  
+  
   <!-- CUSTOM JS -->
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
@@ -197,6 +178,9 @@ include('includes/requester.php');
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
   <script src="resources/js/requester-home.js"></script>
   <script src="resources/js/requester-upload.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 
 </html>
