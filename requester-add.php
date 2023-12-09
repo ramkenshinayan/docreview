17c335c6f3d4521faf53a7846f2014bccc94880f
Mordecai Paragas <2225648@slu.edu.ph>
2023-12-09 23:52:25 +0800

    fix

--- requester-add.php
+++ requester-add.php
@@ -16,13 +16,13 @@ include('includes/requester.php');
   <link rel="icon" type="image/png" href="assets/slu_logo.png">
   <!-- MAIN CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-  <link href="resources/css/user-home.css" rel="stylesheet">
+
+ <link href="resources/css/user-home.css" rel="stylesheet">
   <link href="resources/css/requester-add.css" rel="stylesheet">
 </head>
 
 <body>
-  <nav class="sidebar close">
+<nav class="sidebar close">
     <header>
       <div class="image-text">
         <span class="image">
@@ -94,7 +94,8 @@ include('includes/requester.php');
       </div>
     </div>
     <div class="home-content">
-      <div class="wrapper">
+    
+    <div class="wrapper">
         <form action="includes/upload.php" method="post" enctype="multipart/form-data">
           <div class="file-upload">
             <input class="file-input" type="file" name="file" accept=".doc, .docx, .pdf" hidden>
@@ -103,64 +104,67 @@ include('includes/requester.php');
           </div>
           <button type="submit" name="upload">Submit</button>
         </form>
-        <div>
+       
         <div class="circle-container">
-                        <div class="sequenceTitle">
-                            <p>Select the Sequence of Review</p>
-                        </div>
-                        <?php
-                        $numSections = 5;
-                        for ($i = 1; $i <= $numSections; $i++) {
-                            ?>
-                            <div class="seq">
-                                <div class="circle"><?php echo $i; ?></div>
-                                <div class="btn-group">
-                                <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn office-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onchange="loadReviewers()">
-                            Office names
-                        </button>
-                                    <ul class="dropdown-menu">
-                                              <?php
-                                              $sql = "SELECT DISTINCT officeName FROM organization";
-                                              $result = $conn->query($sql);
+          <div class="sequenceTitle">
+            <p>Select the Sequence of Review</p>
+          </div>
+            <?php
+              $numSections = 5;
+                for ($i = 1; $i <= $numSections; $i++) {
+            ?>
+            <div class="seq">
+            <div class="circle">
+              <?php echo $i; ?></div>
+            <div class="btn-group">
+            <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn office-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onchange="loadReviewers()">
+            Office names
+            </button>
+              <ul class="dropdown-menu">
+                <?php
+                  $sql = "SELECT DISTINCT officeName FROM organization";
+                  $result = $conn->query($sql);
 
-                                              if ($result->num_rows > 0) {
-                                                  while ($row = $result->fetch_assoc()) {
-                                                      $officeName = $row["officeName"];
-                                                      echo "<li><a class='dropdown-item' onclick='updateReviewerText(\"$officeName\", $i)'>$officeName</a></li>";
-                                                  }
-                                              }
-                                              ?>
-                                                </ul>
-                                                </div>
-                                                <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn reviewer-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="reviewerDropdown<?php echo $i; ?>">
-                            Name of Reviewer
-                        </button>
-                        <ul class="dropdown-menu reviewer-dropdown-menu" id="reviewerDropdownMenu<?php echo $i; ?>">
-                            <?php
-                            // Fetch reviewers based on the selected office name
-                            if (isset($officeName)) {
-                              $sqlReviewers = "SELECT firstName, lastName FROM users WHERE email IN (SELECT email FROM organization WHERE officeName = '$officeName')";
-                              $resultReviewers = $conn->query($sqlReviewers);
+                  if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                    $officeName = $row["officeName"];
+                    echo "<li><a class='dropdown-item' onclick='updateReviewerText(\"$officeName\", $i)'>$officeName</a></li>";
+                    }
+                  }
+                ?>
+              </ul>
+                </div>
+                  <button type="button" class="btn btn-secondary dropdown-toggle custom-dropdown-btn reviewer-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="reviewerDropdown<?php echo $i; ?>">
+                    Name of Reviewer
+                  </button>
+                  <ul class="dropdown-menu reviewer-dropdown-menu" id="reviewerDropdownMenu<?php echo $i; ?>">
+                  <?php
+                  // Fetch reviewers based on the selected office name
+                  if (isset($officeName)) {
+                    $sqlReviewers = "SELECT firstName, lastName FROM users WHERE email IN (SELECT email FROM organization WHERE officeName = '$officeName')";
+                    $resultReviewers = $conn->query($sqlReviewers);
                           
-                              if ($resultReviewers->num_rows > 0) {
-                                  while ($rowReviewer = $resultReviewers->fetch_assoc()) {
-                                      $reviewerName = $rowReviewer["firstName"] . ' ' . $rowReviewer["lastName"];
-                                      echo "<li><a class='dropdown-item' onclick='selectReviewer(\"$reviewerName\", $i)'>$reviewerName</a></li>";
-                                  }
-                              }
-                          }
-                            ?>
-                        </ul>
-                    </div>
-                    <?php
+                  if ($resultReviewers->num_rows > 0) {
+                      while ($rowReviewer = $resultReviewers->fetch_assoc()) {
+                      $reviewerName = $rowReviewer["firstName"] . ' ' . $rowReviewer["lastName"];
+                      echo "<li><a class='dropdown-item' onclick='selectReviewer(\"$reviewerName\", $i)'>$reviewerName</a></li>";
+                      }
+                    }
+                  }
+                  ?>
+                </ul>
+                </div>
+                <?php
                 }
-                        ?>
-                    </div>
+                ?>
+                </div>
         </div>
 
         <section class="progress-area"></section>
         <section class="uploaded-area"></section>
       </div>
+
+    </div>
   </section>
 
   <div class="modal fade" id="uploadModal" tabindex="-1" role="dialog" aria-labelledby="uploadModalLabel" aria-hidden="true">
