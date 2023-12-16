6ee581903699fcea1d396ef14f7c048b0ef92238
aeikasajulla <111483292+aeikasajulla@users.noreply.github.com>
2023-12-16 21:28:27 +0800

    retain dark mode

--- resources/js/user-home.js
+++ resources/js/user-home.js
@@ -9,8 +9,32 @@ toggle.addEventListener("click", () => {
 	sidebar.classList.toggle("close");
 });
 
+const setModePreference = (mode) => {
+	localStorage.setItem("modePreference", mode);
+};
+
+const getModePreference = () => {
+	return localStorage.getItem("modePreference");
+};
+
+const applyModePreference = () => {
+	const mode = getModePreference();
+	if (mode === "dark") {
+		body.classList.add("dark");
+		modeText.innerText = "Light Mode";
+	} else {
+		body.classList.remove("dark");
+		modeText.innerText = "Dark Mode";
+	}
+};
+
+applyModePreference();
+
 modeSwitch.addEventListener("click", () => {
 	body.classList.toggle("dark");
+
+	const currentMode = body.classList.contains("dark") ? "dark" : "light";
+	setModePreference(currentMode);
 
 	if (body.classList.contains("dark")) {
 		modeText.innerText = "Light Mode";
